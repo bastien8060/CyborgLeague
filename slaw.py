6ecf1f618a0d2c2942774c7bcfe528fabd8afc96
Bastien Saidi <saidi.ireland@gmail.com>
2022-02-10 10:05:54 +0000

    Added project files

---
+++ slaw.py
@@ -0,0 +1,97 @@
+import base64
+import json
+import os
+import re
+import tempfile
+
+import requests
+from colorama import Fore, Style, init
+
+init()
+
+
+class SLAW:
+    """
+    Simple League API Wrapper
+    """
+
+    def __throw_error(self, error_code:str):
+        print(f"{Fore.RED}{Style.BRIGHT}{error_code}{Style.RESET_ALL}")
+        raise Exception(error_code)
+
+
+
+    def __get_descriptor(self) -> str:
+        """
+        #### Returns:
+            API Descriptor from the LOL lockfile.
+        """
+        lockfile = open(fr"{self.lpath}\lockfile","r",encoding="utf-8")
+        descriptor = re.search(r"(.*?):(.*?):(.*?):(.*?):(.+)", lockfile.read())
+        lockfile.close()
+        return descriptor.groups()
+
+
+
+    def __init_self_ssl(self):
+        req = requests.get(self.__remote_pem_url, allow_redirects=True)
+        open(self.__ssl_cert, 'wb').write(req.content)
+
+
+
+
+    def get_live(self,method:str) -> str:
+        """
+        Query a method supported by the LIVE LoL API.\n
+
+        The difference between `slaw.get_live` and `slaw.get`
+        is that `get_live` uses the API endpoint from the LoL
+        Client on port `2999`, which only runs during a LoL match.
+
+        #### Args:
+          - A Method URL, without a protocol/IP Address, such as: `/api/method/1/value`
+
+        #### Returns:
+          - A Valid JSON Response from the client, parsed into a dict.
+        """
+        port = "2999"
+        protocol = self.__get_descriptor()[4]
+        url = f"{protocol}://127.0.0.1:{port}{method}"
+        auth = {'Authorization': self.authorization}
+        try:
+            response = requests.get(url,verify=self.__ssl_cert, headers=auth).content.decode()
+        except: # pylint: disable=bare-except
+            response = '{"status":"error", "message":"client_not_playing"}'
+        return response
+        #return json.loads(response)
+
+
+
+    def get(self,method:str) -> str:
+        """
+        Query a method supported by the LoL API.
+
+        #### Args:
+          - A Method URL, without a protocol/IP Address, such as: `/api/method/1/value`
+
+        #### Returns:
+          - A Valid JSON Response from the client, parsed into a dict.
+        """
+        port = self.__get_descriptor()[2]
+        protocol = self.__get_descriptor()[4]
+        url = f"{protocol}://127.0.0.1:{port}{method}"
+        authorization = {'Authorization': self.authorization}
+        response = requests.get(url,verify=self.__ssl_cert, headers=authorization).content.decode()
+        return json.loads(response)
+
+
+
+    def __init__(self, lpath=r"C:\Riot Games\League of Legends"):
+        self.lpath = lpath
+        if not os.path.isfile(fr"{self.lpath}\lockfile"):
+            self.__throw_error("League of Legends Lockfile not found. Are you sure LoL is running?")
+        self.__remote_pem_url = "https://static.developer.riotgames.com/docs/lol/riotgames.pem"
+        self.authorization_clear = f"riot:{self.__get_descriptor()[3]}".encode("ascii")
+        self.authorization = f"Basic {base64.b64encode(self.authorization_clear).decode()}"
+        self.__ssl_cert = tempfile.gettempdir()+"cert.pem"
+        self.__init_self_ssl()
---
+++ slaw_vision.py
@@ -0,0 +1,105 @@
+# pylint: skip-file
+import cv2, time, sys
+import win32gui
+import win32ui
+import win32con
+from win32gui import GetWindowText, GetForegroundWindow
+from win32api import GetSystemMetrics
+import numpy as np
+from mss import mss
+import time
+
+while (GetWindowText(GetForegroundWindow()) == "League of Legends (TM) Client"):
+    time.sleep(0.35)
+    print(".",end="")
+    sys.stdout.flush()
+    if (GetWindowText(GetForegroundWindow()) == "League of Legends (TM) Client"):
+        time.sleep(4)
+
+sct = mss()
+monitor_1 = sct.monitors[0]
+
+
+bounding_box = {
+            "top": 0,
+            "left": 0,
+            "width": 1920,
+            "height": 1080,
+        },
+
+def screenshot():
+    w = GetSystemMetrics(0)
+    h = GetSystemMetrics(1)
+
+    sct_original = np.asarray(sct.grab(monitor_1))
+    sct_img = cv2.cvtColor(sct_original, cv2.COLOR_BGRA2BGR)
+    return sct_img
+
+    
+
+def search(__image,__template,__threshold,__style):
+    h, w = __template.shape[:2]
+
+    method = cv2.TM_CCOEFF_NORMED
+
+    res = cv2.matchTemplate(__image, __template, method)
+
+    # fake out max_val for first run through loop
+    max_val = 1
+    prev_min_val, prev_max_val, prev_min_loc, prev_max_loc = None, None, None, None
+    while max_val > __threshold:
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        
+        # Prevent infinite loop. If those 4 values are the same as previous ones, break the loop.
+        if prev_min_val == min_val and prev_max_val == max_val and prev_min_loc == min_loc and prev_max_loc == max_loc:
+            break
+        else:
+            prev_min_val, prev_max_val, prev_min_loc, prev_max_loc = min_val, max_val, min_loc, max_loc
+        
+        if max_val > __threshold:
+            # Prevent start_row, end_row, start_col, end_col be out of range of image
+            start_row = max_loc[1] - h // 2 if max_loc[1] - h // 2 >= 0 else 0
+            end_row = max_loc[1] + h // 2 + 1 if max_loc[1] + h // 2 + 1 <= res.shape[0] else res.shape[0]
+            start_col = max_loc[0] - w // 2 if max_loc[0] - w // 2 >= 0 else 0
+            end_col = max_loc[0] + w // 2 + 1 if max_loc[0] + w // 2 + 1 <= res.shape[1] else res.shape[0]
+
+            res[start_row: end_row, start_col: end_col] = 0
+            #__image = cv2.rectangle(__image,(max_loc[0]+25,max_loc[1]+50), (max_loc[0]+w+1+25, max_loc[1]+h+1+50), __style[0], __style[1] )
+    return __image
+
+
+method = cv2.TM_SQDIFF_NORMED
+
+# Read the images from the file
+turret = cv2.imread('training_data/img/patterns/units/turret.png')
+minion = cv2.imread('training_data/img/patterns/units/minion.png')
+champion_1 = cv2.imread('training_data/img/patterns/units/champion.png')
+champion_2 = cv2.imread('training_data/img/patterns/units/champion_2.png')
+building_1 = cv2.imread('training_data/img/patterns/units/building_1.png')
+building_2 = cv2.imread('training_data/img/patterns/units/building_2.png')
+building_3 = cv2.imread('training_data/img/patterns/units/building_3.jpg')
+
+start = time.time()
+print("now")
+for i in range(3):
+    image = screenshot()
+
+    image = search(image,building_1,0.91,[(0,0,255),4])
+    image = search(image,building_2,0.91,[(0,0,255),4])
+    image = search(image,building_3,0.91,[(0,0,255),4])
+    image = search(image,turret,0.91,[(0,0,255),4])
+    image = search(image,minion,0.93,[(0,255,0),4])
+    image = search(image,champion_1,0.80,[(255,0,255),4])
+    image = search(image,champion_2,0.85,[(255,0,0),4])
+ 
+print("Process time:", (time.time() - start))
+
+
+
+#cv2.imwrite('output.jpg',image)
+# The image is only displayed if we call this
+cv2.imshow('output',image)
+cv2.waitKey(0)
+
+
+
