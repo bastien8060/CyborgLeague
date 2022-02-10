1a789d3eddbc2c2667441def9bf9ad05052f8eb5
Bastien Saidi <saidi.ireland@gmail.com>
2022-02-10 19:07:17 +0000

    Fixed typo in client resize function

--- client/upload.py
+++ client/upload.py
@@ -19,7 +19,7 @@ start = time.time()
 path_img = 'output.jpg'
 url = "http://163.172.97.55:44444"
 
-img = resize(path_img,"rb")
+img = resize(path_img)
 # Requests makes it simple to upload Multipart-encoded files 
 files = {'media': img}
 requests.post(url, files=files)
