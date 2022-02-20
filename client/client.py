1067be4a21e61c1ca022d2bb1ffdb395176a312c
Bastien Saidi <saidi.ireland@gmail.com>
2022-02-20 16:33:17 +0000

    Improved shut-down routine.

--- client/client.py
+++ client/client.py
@@ -1,7 +1,16 @@
 import asyncio
+import signal
+import sys
+
 from CyborgLeague import bot
 
+
+def shutdown_handler(signal, frame):
+    print('Shutting Down... ')
+    sys.exit(0)
+
 def main():
+    signal.signal(signal.SIGINT, shutdown_handler)
     asyncio.run(bot.start())
 
 if __name__ == "__main__":
