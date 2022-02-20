c3dd294a7a8b22731cfed0588082d0e5ce6f7538
Bastien Saidi <saidi.ireland@gmail.com>
2022-02-20 18:53:22 +0000

    Fixed error in splashscreen handler

--- client/client.py
+++ client/client.py
@@ -1,15 +1,22 @@
 import asyncio
 import signal
 import sys
+import os
+import importlib
 
 from CyborgLeague import bot
 
+def splashscreen_handler():
+    import pyi_splash
+    pyi_splash.update_text('UI Loaded ...')
+    pyi_splash.close()
 
 def shutdown_handler(signal, frame):
     print('Shutting Down... ')
     sys.exit(0)
 
 def main():
+    splashscreen_handler()
     signal.signal(signal.SIGINT, shutdown_handler)
     asyncio.run(bot.start())
 
